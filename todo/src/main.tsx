c1b9d2b85102aafc82510da35e7eb6b8ccf1e96f
Sergey Stepanov <Deluta1989@gmail.com>
2025-10-21 18:39:16 +0300

    init: add mini game \lesson 18

--- todo/src/main.tsx
+++ todo/src/main.tsx
@@ -1,12 +1,44 @@
 import {createRoot} from 'react-dom/client'
 import './App.css'
-import {App} from "./app.tsx";
+import {useState} from "react";
+
+
 
 
 createRoot(document.getElementById('root')!).render(
 
-    <App/>
-
+    <Game/>
 
 )
 
+export function Game() {
+   const [activePage, setActivePage] = useState('counter')
+
+    const onFinish = () => {setActivePage('elephant')}
+
+    return <div>
+
+        {activePage === 'counter' && <Counter onFinish = {onFinish}/> }
+        {activePage === 'elephant' && <Elephant/> }
+
+    </div>
+}
+
+
+export function Counter(props) {
+    const [count, setCount] = useState(1)
+    const handleClick= () => {
+        setCount(count + 1)
+        if (count + 1 === 5 && props.onFinish){
+            props.onFinish()
+        }
+
+    }
+    return <div>
+        <button onClick={handleClick}>+ {count}</button>
+    </div>
+}
+
+function Elephant () {
+    return <div style={{fontSize: '100px'}}>🐘</div>
+}
