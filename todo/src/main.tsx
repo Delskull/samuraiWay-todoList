b47607fe802de44bc1eea9768dd3b7caf1e1914a
Sergey Stepanov <Deluta1989@gmail.com>
2025-11-19 10:44:28 +0300

    feat: add customHook lesson 23

--- todo/src/main.tsx
+++ todo/src/main.tsx
@@ -1,11 +1,33 @@
 import {createRoot} from 'react-dom/client'
 import './App.css'
 import {Game} from "./components/Game.tsx";
+import {useEffect, useState} from "react";
 
 
 createRoot(document.getElementById('root')!).render(
+<div>
+<Game/>
 
-   <Game/>
-
+</div>
 )
-
+//lesson 23
+// function useCounter(initNumber:number,ms:number) {
+//     const [value, setValue] = useState(initNumber)
+//     useEffect(() => {
+//         setInterval(() => {
+//             setValue(initNumber)
+//         },ms)
+//     }, []);
+//     return {value,inc:() => setValue(value + 1)}
+// }
+//
+//
+// function Counter() {
+//     const {value,inc} = useCounter(5,5000)
+//     return <div onClick={()=> inc()}>{value}</div>
+// }
+//
+// function Age() {
+//     const {value,inc} = useCounter(10,3000)
+//     return <div>{value} <button  onClick={()=> inc()}>+</button></div>
+// }
