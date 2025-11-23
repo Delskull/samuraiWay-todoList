f824461bf1d9e2b0e0f63320b0db28e9362dd431
Sergey Stepanov <Deluta1989@gmail.com>
2025-11-23 12:37:37 +0300

    add: add customHook / lesson 24

--- todo/src/main.tsx
+++ todo/src/main.tsx
@@ -1,6 +1,33 @@
 import {createRoot} from 'react-dom/client'
 import './App.css'
 import {Users} from "./components/Users.tsx";
+// import {useEffect, useState} from "react";
+//
+//
+//
+// export const Counter = () => {
+//     const { count, inc } = useCounter(3)
+//
+//
+//
+//     return <button onClick={inc}> {count}</button>
+// }
+//
+// const useCounter = (startValue:number = 0) => {
+//     const [count,setCount] = useState(startValue)
+//     const inc = () => {
+//         setCount(count + 1)
+//     }
+//     useEffect(() => {
+//         setInterval(() => {
+//             setCount(startValue)
+//         },3000)
+//     }, []);
+//
+//     return {count, inc}
+// }
+
+
 
 
 createRoot(document.getElementById('root')!).render(
