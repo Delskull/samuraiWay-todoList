a82c63f385e34adec74c4bd577d571d1636c1b70
Sergey Stepanov <Deluta1989@gmail.com>
2025-11-25 19:21:53 +0300

    refactor: add clsx,refactor css, lesson25

--- todo/src/ui/TaskItem.tsx
+++ todo/src/ui/TaskItem.tsx
@@ -1,5 +1,6 @@
 import type {GlobalTaskListItemJsonApiData} from "../dal/api.ts";
 import styles from './TaskItem.module.css'
+import clsx from "clsx";
 
 
 type Props = {
@@ -14,24 +15,37 @@ export function TaskItem({task, isSelected, onTaskSelected}: Props) {
         onTaskSelected(task.id, task.attributes.boardId)
     }
 
-    const className = styles.list + ' ' + (isSelected ? styles.list__item_select : '')
+    const listClassName = clsx({
+        [styles.list]: true,
+        [styles.list__item_select]: isSelected ,
+        [styles.list__item_select_status]: task.attributes.status >= 2 && !isSelected
+
+    })
+    const listItemClassName = clsx({
+        [styles.list__item]: true,
+
+
+    })
+    const listThrowLine = clsx({
+        [styles.list__item_throw]: task.attributes.status >=2
+    })
 
     return <li
         key={task.id}
-        className={className}
+        className={listClassName}
         onClick={handleSelectClick}
     >
-        <div className={styles.list__item}><span className={styles.list__item_content}> Заголовок: </span> <span
-            style={{
-                textDecorationLine: task.attributes.status >= 2 ? 'line-through' : 'none'
-            }}> {task.attributes.title} </span></div>
-        <div className={styles.list__item}><span
-            className={styles.list__item_content}> Статус: </span> {task.attributes.status} <input
+        <div className={listItemClassName }><span className={styles.list__item_content}> Заголовок: </span>
+            <span className={listThrowLine}> {task.attributes.title} </span>
+        </div>
+        <div className={listItemClassName}>Статус:  {task.attributes.status}
+            <input
             type={"checkbox"}
             checked={task.attributes.status >= 2}/>
         </div>
-        <div className={styles.list__item}><span className={styles.list__item_content}> </span> Дата создания
-            задачи: {new Date(task.attributes.addedAt).toLocaleDateString()}</div>
+        <div className={listItemClassName}>
+            Дата создания задачи: {new Date(task.attributes.addedAt).toLocaleDateString()}
+        </div>
 
     </li>
 }
